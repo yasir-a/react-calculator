3de09ed23187672103f201998a0f162601529206
yasir-a <yasir.techhuset@gmail.com>
2025-02-08 00:39:15 +0500

    set up eslint for linting

---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react/configs/recommended.js";
+import importPlugin from "eslint-plugin-import";
+
+export default tseslint.config(
+  { ignores: ["**/node_modules/**", "**/build/**", "**/dist/**"] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactPlugin,
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: { jsx: true },
+      },
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      import: importPlugin,
+      "react-refresh": reactRefresh,
+      prettierConfig,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external"],
+          "newlines-between": "always",
+        },
+      ],
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          project: import.meta.dirname,
+        },
+      },
+      react: { version: "detect" },
+    },
+  },
+  prettierConfig,
+);
