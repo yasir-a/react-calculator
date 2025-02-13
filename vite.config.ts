255de73e6436b1659efd07fa0247c3a12dc60d62
yasir-a <yasir.techhuset@gmail.com>
2025-02-14 01:36:57 +0500

    updated cd, ci and vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), react(), tsconfigPaths()],
   base: "/react-calculator/",
+  build: {
+    outDir: "dist",
+  },
 });
